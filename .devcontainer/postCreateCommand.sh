45f9ee0ce221d3102943637e62ed764e62cf8c0d
Manh Pham <manhpham90vn@icloud.com>
2024-01-02 07:52:22 +0000

    Add devcontainer configuration for Trello Clone API

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo chown -R $USER:$USER /workspace
+
+git clone --depth 1 --branch master https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 --branch master https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
