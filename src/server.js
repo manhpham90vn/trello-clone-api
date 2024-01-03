f418869b9a4345ef89ea6f66cb5f28716ce6bf2f
Manh Pham <manhpham90vn@icloud.com>
2024-01-03 04:01:50 +0000

    Add boardRoute and http-status-codes library

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
 import exitHook from 'async-exit-hook'
-
 import express from 'express'
+
 import { env } from '~/config/environment'
 import { CONNECT_DB, DISCONNECT_DB } from '~/config/mongodb'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
@@ -11,6 +12,8 @@ const START_SERVER = () => {
   app.get('/', async (req, res) => {
     res.end('<h1>Hello World!</h1><hr>')
   })
+
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Server running at http://${env.APP_HOST}:${env.APP_PORT}/`)
